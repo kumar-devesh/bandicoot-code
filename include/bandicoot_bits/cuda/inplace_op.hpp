0e10d39dd2bfeb2a71db77f3fe81e75b497f5a9a
Ryan Curtin <ryan@ratml.org>
2019-08-28 21:22:46 -0400

    Add inplace_op_subview().

--- include/bandicoot_bits/cuda/inplace_op.hpp
+++ include/bandicoot_bits/cuda/inplace_op.hpp
@@ -82,4 +82,49 @@ inplace_op_array(dev_mem_t<eT> dest, dev_mem_t<eT> src, const uword n_elem, kern
   cuCtxSynchronize();
   }
 
+
+
+/**
+ * Run a CUDA kernel on a subview.
+ */
+template<typename eT>
+inline
+void
+inplace_op_subview(dev_mem_t<eT> dest, const eT val, const size_t aux_row1, const size_t aux_col1, const uword n_rows, const uword n_cols, const uword m_n_rows, kernel_id::enum_id num)
+  {
+  coot_extra_debug_sigprint();
+
+  if (n_rows == 0 && n_cols == 0) { return; }
+
+  const uword end_row = aux_row1 + n_rows - 1;
+  const uword end_col = aux_col1 + n_cols - 1;
+
+  // Get kernel.
+  CUfunction kernel = coot_rt.cuda_rt.get_kernel<eT>(num);
+
+  cudaDeviceProp dev_prop;
+  cudaError_t result = cudaGetDeviceProperties(&dev_prop, 0);
+  coot_check_runtime_error( (result != cudaSuccess), "cuda::inplace_op_subview(): couldn't get device properties" );
+
+  const void* args[] = {
+      &(dest.cuda_mem_ptr),
+      &val,
+      (size_t*) &end_row,
+      (size_t*) &end_col,
+      (size_t*) &m_n_rows };
+
+  // TODO: what grid dims?
+  CUresult result2 = cuLaunchKernel(
+      kernel,
+      dev_prop.maxThreadsPerBlock, 1, 1, // grid dims TODO: fix
+      std::ceil((double) n_elem / (double) dev_prop.maxThreadsPerBlock), 1, 1 // block dims
+      0, NULL, // shared mem and stream
+      (void**) args,
+      0);
+
+  coot_check_runtime_error( (result2 != CUDA_SUCCESS), "cuda::inplace_op_subview(): cuLaunchKernel() failed");
+
+  cuCtxSynchronize();
+  }
+
 //! @}
