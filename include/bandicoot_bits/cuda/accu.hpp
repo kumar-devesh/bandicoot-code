da9325567d2b0bcfbfe587d0cb610e7c22ee60a1
Ryan Curtin <ryan@ratml.org>
2020-11-07 23:18:04 -0500

    sasum() and dasum() are sums of *absolute values*.

--- include/bandicoot_bits/cuda/accu.hpp
+++ include/bandicoot_bits/cuda/accu.hpp
@@ -28,95 +28,76 @@ accu(dev_mem_t<eT> mem, const uword n_elem)
 
   coot_debug_check( (get_rt().cuda_rt.is_valid() == false), "cuda runtime not valid" );
 
-  if (std::is_same<eT, float>::value)
-    {
-    float result;
-    cublasStatus_t status = cublasSasum(get_rt().cuda_rt.cublas_handle, n_elem, (float*) mem.cuda_mem_ptr, 1, &result);
+  CUfunction k = get_rt().cuda_rt.get_kernel<eT>(oneway_kernel_id::accu);
+  CUfunction k_small = get_rt().cuda_rt.get_kernel<eT>(oneway_kernel_id::accu_small);
 
-    coot_check_cublas_error( status, "cuda::accu::apply(): call to cublasSasum() failed" );
-    return result;
+  // Compute grid size; ideally we want to use the maximum possible number of threads per block.
+  kernel_dims dims = one_dimensional_grid_dims(n_elem / (2 * std::ceil(std::log2(n_elem))));
+
+  // Create auxiliary memory, with size equal to the number of blocks.
+  Mat<eT> aux(dims.d[0], 1);
+  dev_mem_t<eT> aux_mem = aux.get_dev_mem(false);
+  // Initialize this to the right size, if we will have a second run.
+  Mat<eT> aux2;
+  if (dims.d[0] > 1)
+    {
+    kernel_dims second_dims = one_dimensional_grid_dims(dims.d[0]);
+    aux2.zeros(second_dims.d[0], 1);
     }
-  else if (std::is_same<eT, double>::value)
+  dev_mem_t<eT> aux_mem2 = aux2.get_dev_mem(false);
+  Mat<eT>* out = &aux;
+
+  dev_mem_t<eT>* in_mem = &mem;
+  dev_mem_t<eT>* out_mem = &aux_mem;
+
+  // Each outer iteration will reduce down to the number of blocks.
+  // So, we'll simply keep reducing until we only have one block left.
+  uword in_n_elem = n_elem;
+  do
     {
-    double result;
-    cublasStatus_t status = cublasDasum(get_rt().cuda_rt.cublas_handle, n_elem, (double*) mem.cuda_mem_ptr, 1, &result);
+    // Ensure we always use a power of 2 for the number of threads.
+    const uword num_threads = (uword) std::pow(2.0f, std::ceil(std::log2((float) dims.d[3])));
 
-    coot_check_cublas_error( status, "cuda::accu::apply(): call to cublasDasum() failed" );
-    return result;
-    }
-  else
-    {
-    CUfunction k = get_rt().cuda_rt.get_kernel<eT>(oneway_kernel_id::accu);
-    CUfunction k_small = get_rt().cuda_rt.get_kernel<eT>(oneway_kernel_id::accu_small);
+    const void* args[] = {
+        &(in_mem->cuda_mem_ptr),
+        (uword*) &in_n_elem,
+        &(out_mem->cuda_mem_ptr) };
 
-    // Compute grid size; ideally we want to use the maximum possible number of threads per block.
-    kernel_dims dims = one_dimensional_grid_dims(n_elem / (2 * std::ceil(std::log2(n_elem))));
+    CUresult result = cuLaunchKernel(
+        num_threads < 32 ? k_small : k, // if we have fewer threads than a single warp, we can use a more optimized version of the kernel
+        dims.d[0], dims.d[1], dims.d[2],
+        num_threads, dims.d[4], dims.d[5],
+        2 * num_threads * sizeof(eT), // shared mem should have size equal to number of threads times 2
+        NULL,
+        (void**) args,
+        0);
 
-    // Create auxiliary memory, with size equal to the number of blocks.
-    Mat<eT> aux(dims.d[0], 1);
-    dev_mem_t<eT> aux_mem = aux.get_dev_mem(false);
-    // Initialize this to the right size, if we will have a second run.
-    Mat<eT> aux2;
-    if (dims.d[0] > 1)
+    coot_check_cuda_error(result, "cuda::accu(): cuLaunchKernel() failed");
+
+    if (dims.d[0] == 1)
       {
-      kernel_dims second_dims = one_dimensional_grid_dims(dims.d[0]);
-      aux2.zeros(second_dims.d[0], 1);
+      // We are done.  Terminate.
+      break;
       }
-    dev_mem_t<eT> aux_mem2 = aux2.get_dev_mem(false);
-    Mat<eT>* out = &aux;
 
-    dev_mem_t<eT>* in_mem = &mem;
-    dev_mem_t<eT>* out_mem = &aux_mem;
-
-    // Each outer iteration will reduce down to the number of blocks.
-    // So, we'll simply keep reducing until we only have one block left.
-    uword in_n_elem = n_elem;
-    do
+    in_n_elem = out->n_elem;
+    if (in_mem == &mem)
       {
-      // Ensure we always use a power of 2 for the number of threads.
-      const uword num_threads = (uword) std::pow(2.0f, std::ceil(std::log2((float) dims.d[3])));
+      in_mem = &aux_mem;
+      out_mem = &aux_mem2;
+      }
+    else
+      {
+      std::swap(in_mem, out_mem);
+      out = (out == &aux) ? &aux2 : &aux;
+      }
 
-      const void* args[] = {
-          &(in_mem->cuda_mem_ptr),
-          (uword*) &in_n_elem,
-          &(out_mem->cuda_mem_ptr) };
+    // Now compute sizes for the next iteration.
+    dims = one_dimensional_grid_dims(in_n_elem / (2 * std::ceil(std::log2(in_n_elem))));
 
-      CUresult result = cuLaunchKernel(
-          num_threads < 32 ? k_small : k, // if we have fewer threads than a single warp, we can use a more optimized version of the kernel
-          dims.d[0], dims.d[1], dims.d[2],
-          num_threads, dims.d[4], dims.d[5],
-          2 * num_threads * sizeof(eT), // shared mem should have size equal to number of threads times 2
-          NULL,
-          (void**) args,
-          0);
+    } while (true);
 
-      coot_check_cuda_error(result, "cuda::accu(): cuLaunchKernel() failed");
-
-      if (dims.d[0] == 1)
-        {
-        // We are done.  Terminate.
-        break;
-        }
-
-      in_n_elem = out->n_elem;
-      if (in_mem == &mem)
-        {
-        in_mem = &aux_mem;
-        out_mem = &aux_mem2;
-        }
-      else
-        {
-        std::swap(in_mem, out_mem);
-        out = (out == &aux) ? &aux2 : &aux;
-        }
-
-      // Now compute sizes for the next iteration.
-      dims = one_dimensional_grid_dims(in_n_elem / (2 * std::ceil(std::log2(in_n_elem))));
-
-      } while (true);
-
-    return eT((*out)[0]);
-    }
+  return eT((*out)[0]);
   }
 
 
