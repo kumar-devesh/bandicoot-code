de2326f16b4368eca313fcc462f5491bb9e9ddb8
Ryan Curtin <ryan@ratml.org>
2021-04-22 21:14:00 -0400

    Complete test suite for larfg().

--- tests/larfg.cpp
+++ tests/larfg.cpp
@@ -25,7 +25,7 @@ void test_larfg_manual()
   x.ones();
   Col<eT> x_copy(x);
 
-  const double tau = lapack::larfg(x);
+  const eT tau = lapack::larfg(x);
 
   REQUIRE(abs(tau) <= eT(2));
 
@@ -62,7 +62,7 @@ void test_larfg_zeros()
   Col<eT> x(10);
   x.zeros();
 
-  const double tau = lapack::larfg(x);
+  const eT tau = lapack::larfg(x);
 
   REQUIRE(tau == Approx(eT(0)).margin(1e-6));
 
@@ -75,7 +75,7 @@ void test_larfg_zeros()
   x.zeros();
   x[0] = eT(5);
 
-  const double tau2 = lapack::larfg(x);
+  const eT tau2 = lapack::larfg(x);
 
   REQUIRE(tau2 == Approx(eT(0)).margin(1e-6));
 
@@ -99,7 +99,29 @@ TEST_CASE("larfg_zeros")
 template<typename eT>
 void test_larfg_big()
   {
+  // Use a big random vector.
+  Col<eT> x(1000);
+  x.randu();
+  Col<eT> x_copy(x);
 
+  const eT tau = lapack::larfg(x);
+
+  REQUIRE(abs(tau) <= eT(2));
+
+  // Now, reconstruct H for the test.
+  Mat<eT> H(1000, 1000);
+  H.eye();
+  Col<eT> v_aug = x;
+  v_aug[0] = eT(1);
+  H -= tau * v_aug * v_aug.t();
+
+  Col<eT> out = H * x_copy;
+
+  REQUIRE(((eT) out[0]) == Approx((eT) x[0]));
+  for (uword i = 1; i < 1000; ++i)
+    {
+    REQUIRE(abs(out[i]) < eT(1e-5));
+    }
   }
 
 
@@ -112,11 +134,42 @@ TEST_CASE("larfg_big")
 
 
 
-
+// This test will not work until dot() can support underflow and safely scale.
+/*
 template<typename eT>
 void test_larfg_small_norm()
   {
+  // Create a vector with a very small norm.
+  eT min_val = 10000 * std::numeric_limits<eT>::min();
 
+  // All elements will be some multiple of that minimum value.
+  Col<eT> x(500);
+  for (uword i = 0; i < x.n_elem; ++i)
+    {
+    x[i] = (rand() % 100) * min_val;
+    }
+
+  Col<eT> x_copy(x);
+
+  const eT tau = lapack::larfg(x);
+
+  REQUIRE(abs(tau) <= eT(2));
+
+  // Now reconstruct H for the test.
+  Mat<eT> H(500, 500);
+  H.eye();
+  Col<eT> v_aug = x;
+  v_aug[0] = eT(1);
+  H -= tau * v_aug * v_aug.t();
+
+  Col<eT> out = H * x_copy;
+
+  REQUIRE(((((eT) x[0]) != eT(0.0)) && (((eT) x[0]) != eT(-0.0))));
+  REQUIRE(((eT) out[0]) == Approx((eT) x[0]));
+  for (uword i = 1; i < 500; ++i)
+    {
+    REQUIRE(abs(out[i]) < eT(1e-5));
+    }
   }
 
 
@@ -125,4 +178,48 @@ TEST_CASE("larfg_small_norm")
   {
   test_larfg_small_norm<float>();
   test_larfg_small_norm<double>();
+  }
+*/
+
+
+
+template<typename eT>
+void test_larfg_one_elem()
+  {
+  Col<eT> x(1);
+  x[0] = eT(3);
+
+  const eT tau = lapack::larfg(x);
+
+  REQUIRE(tau == Approx(eT(0)).margin(1e-20));
+
+  REQUIRE(eT(x[0]) == Approx(eT(3)));
+  }
+
+
+
+TEST_CASE("larfg_one_elem")
+  {
+  test_larfg_one_elem<float>();
+  test_larfg_one_elem<double>();
+  }
+
+
+
+template<typename eT>
+void test_larfg_empty()
+  {
+  Col<eT> x(0);
+
+  const eT tau = lapack::larfg(x);
+
+  REQUIRE(tau == Approx(eT(0)).margin(1e-20));
+  }
+
+
+
+TEST_CASE("larfg_empty")
+  {
+  test_larfg_empty<float>();
+  test_larfg_empty<double>();
   }
