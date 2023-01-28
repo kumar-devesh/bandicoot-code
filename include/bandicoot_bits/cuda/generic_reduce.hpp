c6228ad315ca9cdfc254587ccb699956f18bebfa
Ryan Curtin <ryan@ratml.org>
2023-01-28 12:12:02 -0500

    Refactor generic_reduce() to be more generic.

--- include/bandicoot_bits/cuda/generic_reduce.hpp
+++ include/bandicoot_bits/cuda/generic_reduce.hpp
@@ -14,92 +14,194 @@
 
 
 
-// Utility to run a full reduce in an efficient way.
+// Utility to run a full reduce in an efficient and generic way.
 //
-// Note that all kernels that are used with generic reduce are expected to return an eT,
+// Note that all kernels that are used with generic reduce are expected to compute an eT,
 // and the first three arguments should be:
 //  - const eT1* mem
 //  - const UWORD n_elem
 //  - eT1* out_mem
 // Additional arguments are fine, so long as those first three are the same.
 
+// This version uses the same kernel for all reduce passes.
 template<typename eT, typename... Args>
 inline
 eT
-generic_reduce(CUfunction& kernel,         // kernel for full-scale reduction passes
-               CUfunction& kernel_small,   // kernel for small reductions
-               const char* kernel_name,    // for error reporting
-               const dev_mem_t<eT> mem,    // initial memory that we are reducing
-               const uword n_elem,         // size of memory we are reducing
-               Args... extra_args)         // any extra arguments to kernels; should be references, not pointers
+generic_reduce(const dev_mem_t<eT> mem,
+               const uword n_elem,
+               const char* kernel_name,
+               CUfunction& kernel,
+               CUfunction& kernel_small,
+               std::tuple<Args...>& kernel_extra_args)
   {
-  // Compute grid size; ideally we want to use the maximum possible number of threads per block.
-  kernel_dims dims = one_dimensional_grid_dims(std::ceil(n_elem / (2 * std::ceil(std::log2(n_elem)))));
+  return generic_reduce(mem,
+                        n_elem,
+                        kernel_name,
+                        kernel,
+                        kernel_small,
+                        kernel_extra_args,
+                        kernel,
+                        kernel_small,
+                        kernel_extra_args);
+  }
 
-  // Create auxiliary memory, with size equal to the number of blocks.
-  Mat<eT> aux(dims.d[0], 1);
-  dev_mem_t<eT> aux_mem = aux.get_dev_mem(false);
-  // Initialize this to the right size, if we will have a second run.
-  Mat<eT> aux2;
-  if (dims.d[0] > 1)
+
+
+template<typename eT, typename... A1, typename... A2>
+inline
+eT
+generic_reduce(const dev_mem_t<eT> mem,
+               const uword n_elem,
+               const char* kernel_name,
+               CUfunction& first_kernel,
+               CUfunction& first_kernel_small,
+               std::tuple<A1...>& first_kernel_extra_args,
+               CUfunction& second_kernel,
+               CUfunction& second_kernel_small,
+               std::tuple<A2...>& second_kernel_extra_args)
+  {
+  // Do first pass, hand off to appropriate smaller reduce if needed.
+  // The first pass will use the first kernel; subsequent passes use the second kernel.
+  const size_t mtpb = (size_t) get_rt().cuda_rt.dev_prop.maxThreadsPerBlock;
+
+  // Compute size of auxiliary memory.
+  const size_t aux_mem_size = (n_elem + mtpb - 1) / mtpb;
+  Mat<eT> aux_mem(aux_mem_size);
+  generic_reduce_inner(mem,
+                       n_elem,
+                       aux_mem.get_dev_mem(false),
+                       kernel_name,
+                       first_kernel,
+                       first_kernel_small,
+                       first_kernel_extra_args,
+                       second_kernel,
+                       second_kernel_small,
+                       second_kernel_extra_args);
+  return eT(aux_mem[0]);
+  }
+
+
+
+// unpack_args is a metaprogramming utility to recursively iterate over the extra arguments for a kernel
+
+template<size_t i, typename... Args>
+struct
+unpack_args
+  {
+  inline void apply(void** args, std::tuple<Args...>& args_tuple)
     {
-    kernel_dims second_dims = one_dimensional_grid_dims(dims.d[0]);
-    aux2.zeros(second_dims.d[0], 1);
+    args[3 + i] = &std::get<i>(args_tuple);
+    unpack_args<i - 1, Args...>::apply(args, args_tuple);
     }
-  dev_mem_t<eT> aux_mem2 = aux2.get_dev_mem(false);
-  Mat<eT>* out = &aux;
+  };
 
-  dev_mem_t<eT>* in_mem = (dev_mem_t<eT>*) &mem;
-  dev_mem_t<eT>* out_mem = &aux_mem;
 
-  // Each outer iteration will reduce down to the number of blocks.
-  // So, we'll simply keep reducing until we only have one block left.
-  uword in_n_elem = n_elem;
-  do
+
+template<typename... Args>
+struct
+unpack_args<0, Args...>
+  {
+  inline void apply(void** args, std::tuple<Args...>& args_tuple)
     {
-    // Ensure we always use a power of 2 for the number of threads.
-    const uword num_threads = (uword) std::pow(2.0f, std::ceil(std::log2((float) dims.d[3])));
+    // This is the last iteration of the recursion.
+    args[3] = &std::get<0>(args_tuple);
+    }
+  };
 
-    const void* args[] = {
-        &(in_mem->cuda_mem_ptr),
-        (uword*) &in_n_elem,
-        &(out_mem->cuda_mem_ptr),
-        &extra_args... };
+
+
+template<typename eT, typename... A1, typename... A2>
+inline
+void
+generic_reduce_inner(const dev_mem_t<eT> mem,
+                     const uword n_elem,
+                     dev_mem_t<eT> aux_mem,
+                     const char* kernel_name,
+                     CUfunction& first_kernel,
+                     CUfunction& first_kernel_small,
+                     std::tuple<A1...>& first_kernel_extra_args,
+                     CUfunction& second_kernel,
+                     CUfunction& second_kernel_small,
+                     std::tuple<A2...>& second_kernel_extra_args)
+  {
+  const size_t mtpb = (size_t) get_rt().cuda_rt.dev_prop.maxThreadsPerBlock;
+
+  if (n_elem <= mtpb)
+    {
+    // If the data is small enough, we can do the work in a single pass.
+    generic_reduce_inner_small(mem,
+                               n_elem,
+                               aux_mem,
+                               kernel_name,
+                               first_kernel,
+                               first_kernel_small,
+                               first_kernel_extra_args);
+    }
+  else
+    {
+    // Here, we will have to do multiple reduces.
+    const size_t block_size = (n_elem + mtpb - 1) / mtpb;
+
+    void* args[3 + sizeof...(A1)];
+    args[0] = &mem.cuda_mem_ptr;
+    args[1] = &n_elem;
+    args[2] = &aux_mem.cuda_mem_ptr;
+    unpack_args<sizeof...(A1) - 1, A1...>::apply(args, first_kernel_extra_args);
 
     CUresult result = cuLaunchKernel(
-        num_threads <= 32 ? kernel_small : kernel, // if we have fewer threads than a single warp, we can use a more optimized version of the kernel
-        dims.d[0], dims.d[1], dims.d[2],
-        num_threads, dims.d[4], dims.d[5],
-        2 * num_threads * sizeof(eT), // shared mem should have size equal to number of threads times 2
+        first_kernel,
+        block_size, 1, 1, mtpb, 1, 1,
+        2 * mtpb * sizeof(eT), // shared mem should have size equal to number of threads times 2
         NULL,
         (void**) args,
         0);
 
-    coot_check_cuda_error( result, std::string("coot::cuda::") + std::string(kernel_name) + std::string(": cuLaunchKernel() failed") );
+    coot_check_cuda_error(result, std::string("coot::cuda::") + std::string(kernel_name) + std::string(": cuLaunchKernel() failed"));
 
-    if (dims.d[0] == 1)
-      {
-      // We are done.  Terminate.
-      break;
-      }
+    // Now, take subsequent passes.
+    // We use the second kernel for all subsequent passes.
+    generic_reduce_inner(mem,
+                         n_elem,
+                         aux_mem,
+                         kernel_name,
+                         second_kernel,
+                         second_kernel_small,
+                         second_kernel_extra_args,
+                         second_kernel,
+                         second_kernel_small,
+                         second_kernel_extra_args);
+    }
+  }
 
-    in_n_elem = out->n_elem;
-    if (in_mem == &mem)
-      {
-      in_mem = &aux_mem;
-      out_mem = &aux_mem2;
-      out = &aux2;
-      }
-    else
-      {
-      std::swap(in_mem, out_mem);
-      out = (out == &aux) ? &aux2 : &aux;
-      }
 
-    // Now compute sizes for the next iteration.
-    dims = one_dimensional_grid_dims(std::ceil(in_n_elem / (2 * std::ceil(std::log2(in_n_elem)))));
 
-    } while (true);
+template<typename eT, typename... Args>
+inline
+void
+generic_reduce_inner_small(const dev_mem_t<eT> mem,
+                           const uword n_elem,
+                           dev_mem_t<eT> aux_mem, // must have at least one element
+                           const char* kernel_name,
+                           CUfunction& kernel,
+                           CUfunction& kernel_small, // for 32 threads or fewer
+                           std::tuple<Args...>& kernel_extra_args)
+  {
+  // TODO: can this be made more efficient?
+  const uword num_threads = (uword) std::pow(2.0f, std::ceil(std::log2((float) n_elem)));
 
-  return eT((*out)[0]);
+  void* args[3 + sizeof...(Args)];
+  args[0] = &mem.cuda_mem_ptr;
+  args[1] = &n_elem;
+  args[2] = &aux_mem.cuda_mem_ptr;
+  unpack_args<sizeof...(Args) - 1, Args...>::apply(args, kernel_extra_args);
+
+  CUresult result = cuLaunchKernel(
+      num_threads <= 32 ? kernel_small : kernel, // if we have fewer threads than a single warp, we can use a more optimized version of the kernel
+      1, 1, 1, num_threads, 1, 1,
+      2 * num_threads * sizeof(eT), // shared mem should have size equal to number of threads times 2
+      NULL,
+      (void**) args,
+      0);
+
+  coot_check_cuda_error(result, std::string("coot::cuda::") + std::string(kernel_name) + std::string(": cuLaunchKernel() failed"));
   }
