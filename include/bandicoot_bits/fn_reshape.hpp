134dd78e8524f352773431b7faeebb3d4e61a83c
Ryan Curtin <ryan@ratml.org>
2023-03-08 17:08:57 -0500

    Add reshape() function.

---
+++ include/bandicoot_bits/fn_reshape.hpp
@@ -0,0 +1,29 @@
+// Copyright 2023 Ryan Curtin (http://www.ratml.org/)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ------------------------------------------------------------------------
+
+
+
+template<typename T1>
+coot_warn_unused
+inline
+typename enable_if2<
+  is_coot_type<T1>::value,
+  const Op<T1, op_reshape>
+>::result
+reshape(const T1& x, const uword in_n_rows, const uword in_n_cols)
+  {
+  coot_extra_debug_sigprint();
+
+  return Op<T1, op_reshape>(x, in_n_rows, in_n_cols);
+  }
