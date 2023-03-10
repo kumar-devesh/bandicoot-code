a7b464d5c86c5102fb1a428c102d1a402f8e3efb
Ryan Curtin <ryan@ratml.org>
2023-03-10 10:07:06 -0500

    Add handling for row-wise and column-wise all().

--- include/bandicoot_bits/mtop_all_meat.hpp
+++ include/bandicoot_bits/mtop_all_meat.hpp
@@ -14,13 +14,99 @@
 
 
 
-template<typename out_eT, typename T1>
+template<typename T1>
 inline
 void
-mtop_all::apply(Mat<out_eT>& out, const mtOp<out_eT, T1, mtop_all>& in)
+mtop_all::apply(Mat<uword>& out, const mtOp<uword, T1, mtop_all>& in)
   {
   coot_extra_debug_sigprint();
 
+  const uword dim = in.aux_uword;
+
+  coot_debug_check( (dim > 1), "all(): parameter 'dim' must be 0 or 1" );
+
+  unwrap<T1> U(in.q);
+
+  // Shortcut if the input is empty.
+  if (U.M.n_elem == 0)
+    {
+    if (dim == 0)
+      {
+      out.set_size(1, 0);
+      }
+    else
+      {
+      out.set_size(0, 1);
+      }
+
+    return;
+    }
+
+  apply_direct(out, U.M, dim);
+  }
+
+
+
+inline
+void
+mtop_all::apply_direct(Mat<uword>& out, const Mat<uword>& in, const uword dim)
+  {
+  coot_extra_debug_sigprint();
+
+  if (&out == &in)
+    {
+    // For aliases, we have to output into a temporary matrix.
+    Mat<uword> tmp;
+    apply_direct(tmp, in, dim);
+    out.steal_mem(tmp);
+    return;
+    }
+
+  if (dim == 0)
+    {
+    out.set_size(1, in.n_cols);
+    coot_rt_t::all(out.get_dev_mem(false), in.get_dev_mem(false), in.n_rows, in.n_cols, uword(0), twoway_kernel_id::rel_all_neq_colwise, true);
+    }
+  else
+    {
+    out.set_size(in.n_rows, 1);
+    coot_rt_t::all(out.get_dev_mem(false), in.get_dev_mem(false), in.n_rows, in.n_cols, uword(0), twoway_kernel_id::rel_all_neq_rowwise, false);
+    }
+  }
+
+
+
+template<typename eT>
+inline
+void
+mtop_all::apply_direct(Mat<uword>& out, const Mat<eT>& in, const uword dim)
+  {
+  coot_extra_debug_sigprint();
+
+  if (dim == 0)
+    {
+    out.set_size(1, in.n_cols);
+    coot_rt_t::all(out.get_dev_mem(false), in.get_dev_mem(false), in.n_rows, in.n_cols, eT(0), twoway_kernel_id::rel_all_neq_colwise, true);
+    }
+  else
+    {
+    out.set_size(in.n_rows, 1);
+    coot_rt_t::all(out.get_dev_mem(false), in.get_dev_mem(false), in.n_rows, in.n_cols, eT(0), twoway_kernel_id::rel_all_neq_rowwise, false);
+    }
+  }
+
+
+
+template<typename eT>
+inline
+void
+mtop_all::apply_direct(Mat<uword>& out, const subview<eT>& in, const uword dim)
+  {
+  coot_extra_debug_sigprint();
+
+  // Subviews must be extracted beforehand, and then we use the regular Mat implementation.
+  Mat<eT> tmp(in);
+  apply_direct(out, tmp, dim);
   }
 
 
