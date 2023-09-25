20630896dd37c8c78c056f4475c039427d540695
Ryan Curtin <ryan@ratml.org>
2023-09-25 10:28:21 -0400

    Add tests for two-object min() and max().

--- tests/max.cpp
+++ tests/max.cpp
@@ -448,3 +448,137 @@ TEMPLATE_TEST_CASE("subview_max_rowwise_full", "[max]", float, double, u32, s32,
     REQUIRE( eT(s[r]) == Approx(eT(r)) );
     }
   }
+
+
+
+TEMPLATE_TEST_CASE("two_mat_max_simple", "[max]", float, double, u32, s32, u64, s64)
+  {
+  typedef TestType eT;
+
+  if (!coot_rt_t::is_supported_type<eT>())
+    {
+    return;
+    }
+
+  Mat<eT> x(50, 50);
+  x.zeros();
+  Mat<eT> y(50, 50);
+  y.ones();
+
+  Mat<eT> z = max(x, y);
+
+  REQUIRE( z.n_rows == 50 );
+  REQUIRE( z.n_cols == 50 );
+
+  REQUIRE( all( all( z == eT(1) ) ) );
+  }
+
+
+
+TEMPLATE_TEST_CASE("two_mat_max_expr", "[max]", float, double, u32, s32, u64, s64)
+  {
+  typedef TestType eT;
+
+  if (!coot_rt_t::is_supported_type<eT>())
+    {
+    return;
+    }
+
+  Mat<eT> x(100, 100, fill::randu);
+  Mat<eT> y(100, 100, fill::randn);
+
+  Mat<eT> z = max(x.t(), (3 * y + 1));
+
+  Mat<eT> x_expr = x.t();
+  Mat<eT> y_expr = (3 * y + 1);
+  Mat<eT> z_expr = max(x_expr, y_expr);
+
+  REQUIRE( z.n_rows == 100 );
+  REQUIRE( z.n_cols == 100 );
+
+  REQUIRE( all( all( z == z_expr ) ) );
+  }
+
+
+
+TEMPLATE_TEST_CASE(
+  "two_mat_max_conv_to_inputs",
+  "[max]",
+  (std::pair<double, float>), (std::pair<double, u32>), (std::pair<double, s32>), (std::pair<double, u64>), (std::pair<double, s64>),
+  (std::pair<float, double>), (std::pair<float, u32>), (std::pair<float, s32>), (std::pair<float, u64>), (std::pair<float, s64>),
+  (std::pair<u32, double>), (std::pair<u32, float>), (std::pair<u32, s32>), (std::pair<u32, u64>), (std::pair<u32, s64>),
+  (std::pair<s32, double>), (std::pair<s32, float>), (std::pair<s32, u32>), (std::pair<s32, u64>), (std::pair<s32, s64>),
+  (std::pair<u64, double>), (std::pair<u64, float>), (std::pair<u64, u32>), (std::pair<u64, s32>), (std::pair<u64, s64>),
+  (std::pair<s64, double>), (std::pair<s64, float>), (std::pair<s64, u32>), (std::pair<s64, s32>), (std::pair<s64, u64>))
+  {
+  typedef typename TestType::first_type eT1;
+  typedef typename TestType::second_type eT2;
+
+  if (!coot_rt_t::is_supported_type<eT1>() || !coot_rt_t::is_supported_type<eT2>())
+    {
+    return;
+    }
+
+  Mat<eT1> in_a(150, 150, fill::randu);
+  Mat<eT2> in_b(150, 150, fill::randu);
+
+  Mat<eT1> out = max(in_a, conv_to<Mat<eT1>>::from(in_b));
+
+  Mat<eT1> in_b_conv = conv_to<Mat<eT1>>::from(in_b);
+  Mat<eT1> out_ref = max(in_a, in_b_conv);
+
+  REQUIRE( out.n_rows == out_ref.n_rows );
+  REQUIRE( out.n_cols == out_ref.n_cols );
+  REQUIRE( all( all( out == out_ref ) ) );
+
+  Mat<eT2> out2 = max(conv_to<Mat<eT2>>::from(in_a), in_b);
+
+  Mat<eT2> in_a_conv = conv_to<Mat<eT2>>::from(in_a);
+  Mat<eT2> out2_ref = max(in_a_conv, in_b);
+
+  REQUIRE( out2.n_rows == out2_ref.n_rows );
+  REQUIRE( out2.n_cols == out2_ref.n_cols );
+  REQUIRE( all( all( out2 == out2_ref ) ) );
+
+  Mat<eT2> out3 = max(conv_to<Mat<eT2>>::from(in_a), conv_to<Mat<eT2>>::from(in_b_conv));
+
+  Mat<eT2> in_b_conv2 = conv_to<Mat<eT2>>::from(in_b_conv);
+  Mat<eT2> out3_ref = max(in_a_conv, in_b_conv2);
+
+  REQUIRE( out3.n_rows == out3_ref.n_rows );
+  REQUIRE( out3.n_cols == out3_ref.n_cols );
+  REQUIRE( all( all( out3 == out3_ref ) ) );
+  }
+
+
+
+TEMPLATE_TEST_CASE(
+  "two_mat_max_conv_to_outputs",
+  "[max]",
+  (std::pair<double, float>), (std::pair<double, u32>), (std::pair<double, s32>), (std::pair<double, u64>), (std::pair<double, s64>),
+  (std::pair<float, double>), (std::pair<float, u32>), (std::pair<float, s32>), (std::pair<float, u64>), (std::pair<float, s64>),
+  (std::pair<u32, double>), (std::pair<u32, float>), (std::pair<u32, s32>), (std::pair<u32, u64>), (std::pair<u32, s64>),
+  (std::pair<s32, double>), (std::pair<s32, float>), (std::pair<s32, u32>), (std::pair<s32, u64>), (std::pair<s32, s64>),
+  (std::pair<u64, double>), (std::pair<u64, float>), (std::pair<u64, u32>), (std::pair<u64, s32>), (std::pair<u64, s64>),
+  (std::pair<s64, double>), (std::pair<s64, float>), (std::pair<s64, u32>), (std::pair<s64, s32>), (std::pair<s64, u64>))
+  {
+  typedef typename TestType::first_type eT1;
+  typedef typename TestType::second_type eT2;
+
+  if (!coot_rt_t::is_supported_type<eT1>() || !coot_rt_t::is_supported_type<eT2>())
+    {
+    return;
+    }
+
+  Mat<eT1> in_a(150, 150, fill::randu);
+  Mat<eT1> in_b(150, 150, fill::randu);
+
+  Mat<eT2> out = conv_to<Mat<eT2>>::from(max(in_a, in_b));
+
+  Mat<eT1> out_pre_conv = max(in_a, in_b);
+  Mat<eT2> out_ref = conv_to<Mat<eT2>>::from(out_pre_conv);
+
+  REQUIRE( out.n_rows == out_ref.n_rows );
+  REQUIRE( out.n_cols == out_ref.n_cols );
+  REQUIRE( all( all( out == out_ref ) ) );
+  }
